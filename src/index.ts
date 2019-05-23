8c7e4086ab1f5e7bceec02cb893b45481ba591a1
Andy Korek <andy.korek@gmail.com>
2019-05-23 20:23:42 +0200

    disabled bootstrap  material by default

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import '../src/sass/main.scss';
 import '../src/ts/custom'
 import '../src/ts/vendor';
 require('materialize-loader!./cfg/materialize.config.js');
-require('bootstrap-material-design');
+// require('bootstrap-material-design');
