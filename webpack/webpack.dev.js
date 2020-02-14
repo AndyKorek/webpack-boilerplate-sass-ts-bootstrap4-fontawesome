86dd6e3dad3b19a422f38b9a0d265272f9326cdc
Andy Korek <andy.korek@gmail.com>
2020-02-14 17:35:08 +0100

    added Stylelint, removed Material Icons,removed Fonts LaBelle And Roboto Mono,moved Browserlist to package.json, code Cleanup, optimized Production Build

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 module.exports = merge(common, {
@@ -11,4 +12,12 @@ module.exports = merge(common, {
         port: 9100,
         overlay: true
     },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: 'css/[id].css' ,
+        }),
+
+    ],
 });
